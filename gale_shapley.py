85135d0bb5197be30e8503bbb2d0ae8dbdf0199e
smur232 <sarah.d.murata@gmail.com>
2015-07-31 19:55:20 +1200

    reads from a file and outputs a file - correct algorithm

--- gale_shapley.py
+++ gale_shapley.py
@@ -1,55 +1,65 @@
 class Person:
     def __init__(self, preference):
         self.preference = preference
-        self.queue = preference
+        self.hasnt_prosed_to = list(reversed(preference))
         self.takenTo = False
 
 def gale_shapley():
-    n = int(input())
+    file = open('input.txt', 'r')
+    outputfile = open('myoutput.txt','w')
+    n = file.readline()
+    n = int(n)
     instance = 0
     output = ''
+
     while n != 0:
         instance += 1
+        blue_nodes = [x+1 for x in range(n)]
+        bachelor = blue_nodes.copy()
+
         blue_node_preferences = {}
         pink_node_preferences = {}
-        bachelor = [x+1 for x in range(n)]
-        blue_nodes = [x+1 for x in range(n)]
-
         for i in range(n):
-            preference = input().split()
-            preference_list = list(map(int, preference))
+            preference = file.readline().split()
+            preference_list = list(map(int,preference))
             blue_node_preferences[i+1] = Person(preference_list)
 
         for i in range(n):
-            preference = input().split()
-            pink_node_preferences[i+1] = Person(list(map(int, preference)))
+            preference = file.readline().split()
+            preference_list = list(map(int,preference))
+            pink_node_preferences[i+1] = Person(preference_list)
+
+        # print(blue_node_preferences[0])
+        # print(pink_node_preferences[0])
 
         taken_nodes = set()
-        taken_pink_nodes = 0
 
-        while taken_pink_nodes < n:
+        while bachelor:
             for blue_node in bachelor:
-                unproposed = blue_node_preferences[blue_node].queue[0]
+                unproposed = blue_node_preferences[blue_node].hasnt_prosed_to.pop()
                 if unproposed not in taken_nodes:
                     pink_node_preferences[unproposed].takenTo = blue_node
                     blue_node_preferences[blue_node].takenTo = unproposed
                     taken_nodes.add(unproposed)
                     bachelor.remove(blue_node)
-                    taken_pink_nodes += 1
 
                 elif unproposed in taken_nodes:
                     nodeTakenTo = pink_node_preferences[unproposed].takenTo
                     if pink_node_preferences[unproposed].preference.index(blue_node) < pink_node_preferences[unproposed].preference.index(nodeTakenTo):
                         pink_node_preferences[unproposed].takenTo = blue_node
                         blue_node_preferences[blue_node].takenTo = unproposed
+                        blue_node_preferences[nodeTakenTo].takenTo = False
+                        bachelor.remove(blue_node)
                         bachelor.append(nodeTakenTo)
 
-                blue_node_preferences[blue_node].queue.remove(unproposed)
-                blue_node_preferences[blue_node].queue.append(unproposed)
         output = output + 'Instance ' + str(instance) + ':\n'
         for blue_node in blue_nodes:
             output = output + str(blue_node_preferences[blue_node].takenTo) + '\n'
-        n = int(input())
-    print(output[:-1])
+        n = file.readline()
+        n = int(n)
+
+    outputfile.write(output[:-1])
+    outputfile.close()
+    file.close()
 
 gale_shapley()
